4bc8fab857874b0d601c31af1322b31ea30ed33d
baibhav.pradhan <b.pradhan@astrasolutions.com.np>
2024-03-03 14:44:37 +0545

    click events

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,9 +1,27 @@
 const Home = () => {
   const title = "Home"
 
+  const handleClick = () => {
+    console.log('hellow')
+  }
+  // without any arguement
+
+  const handleClickWithParam = (name) => console.log(`Hello ${name}!`)
+  // with arguement
+
+  // using the event object
+  const handleClickWithEvent = (event) => console.log(event)
+
   return (
     <div className="content">
       <h1>{title}</h1>
+      <div className="button-actions">
+        <button onClick={handleClick}>Click without parameter</button>
+        {/* cannot use handleClick() as it would invoke the function without even taking place */}
+        <button onClick={() => handleClickWithParam('mario')}>With Parameter</button>
+
+        <button onClick={(e) => handleClickWithEvent(e)}>With Event Object</button>
+      </div>
     </div>
   );
 }
